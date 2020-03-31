bf6991bac686d1c4ef72b2625de47e7e2cfbef10
Andy Nixon <andielane@yahoo.com>
2020-03-31 12:54:01 -0700

    Adding Input component

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [todoItem, setTodoItem] = useState('');
   return (
     <View style={styles.container}>
-      <Text>This Will be A To Do List.</Text>
+      <TextInput 
+        style={{height: 40}}
+        placeholder={"I am holding your place"}
+        onChangeText={todoItem => setTodoItem(todoItem)}
+        defaultValue={ todoItem }
+      />
+      <Text>
+        {todoItem}
+      </Text>
     </View>
   );
 }
@@ -17,3 +26,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
